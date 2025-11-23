8a9f9f819198f11e9bd1bc11e02ce0fe94878484
HalfToothed <abhishekandmails@gmail.com>
2025-11-23 23:08:13 +0530

    feat: undo & redo

--- Counter/script.js
+++ Counter/script.js
@@ -1,15 +1,43 @@
+document.getElementById("increment").addEventListener("click", increment);
+document.getElementById("decrement").addEventListener("click", decrement);
+document.getElementById("undo").addEventListener("click", handleUndo);
+document.getElementById("redo").addEventListener("click", handleRedo);
+
 let counter = document.getElementById("count");
 let count = 0
+let undoStack = []
+let redoStack = []
 
 function increment(){
-    count = count + 1
+    undoStack.push(count)
+    count = count + 1   
     counter.innerHTML = count
 }
 
 function decrement(){
+    undoStack.push(count)
     if (count > 0){
         count = count -1
         counter.innerHTML = count
     }
-    
+}
+
+function handleUndo(){
+    let value = undoStack.pop()
+    if(value != undefined) 
+    {
+        redoStack.push(count)
+        counter.innerHTML = value
+        count = value
+    }   
+}
+
+function handleRedo(){
+    let value = redoStack.pop()
+    if(value != undefined){
+
+        undoStack.push(count)
+        counter.innerHTML = value
+        count = value
+    }
 }
