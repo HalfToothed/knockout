74b1232e0666fa830674b788bcf1caba2c62ba47
HalfToothed <abhishekandmails@gmail.com>
2025-12-08 00:22:21 +0530

    feat: add and remove from DOM functionality added

--- countdown/script.js
+++ countdown/script.js
@@ -1,15 +1,19 @@
 let title = "";
 let text = "";
 let date = "";
+let list = "";
 
 let allData = []
+let id = 0
 
 function submitButton() {
   title = document.getElementById("title").value;
   text = document.getElementById("text").value;
   date = document.getElementById("date").value;
+  id += 1
 
   let person = {
+    Id : id,
     Title : title,
     Des: text,
     Date: date
@@ -17,6 +21,27 @@ function submitButton() {
 
   allData.push(person);
 
-  console.log(allData);
+  list = document.getElementById("list")
+
+  let obj = ""
+  
+  for(var i in allData){
+    obj += `<li id=${allData[i].Id}>
+                <span>${allData[i].Title}</span>
+                <button onClick="Remove(${allData[i].Id})">Remove</button>
+            </li>`    
+  }
+
+  list.innerHTML = obj
+
+  console.log(allData)
+
 }
 
+function Remove(num){
+    let l = document.getElementById(num)
+    list.removeChild(l);
+
+    let ind = allData.findIndex(v => v.Id === num);
+    allData.splice(ind, 1);
+}
