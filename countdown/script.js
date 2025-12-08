ee6ddd65d4abcc4a270376d4cdec5938ae84fb95
HalfToothed <abhishekandmails@gmail.com>
2025-12-08 23:45:16 +0530

    feat: managed dates and parsing

--- countdown/script.js
+++ countdown/script.js
@@ -2,46 +2,90 @@ let title = "";
 let text = "";
 let date = "";
 let list = "";
+let appDiv = document.getElementById("app")
+let countdownDiv = document.getElementById("countdown") 
 
-let allData = []
-let id = 0
+
+let allData = [];
+let id = 0;
 
 function submitButton() {
   title = document.getElementById("title").value;
   text = document.getElementById("text").value;
   date = document.getElementById("date").value;
-  id += 1
+  id += 1;
 
   let person = {
-    Id : id,
-    Title : title,
+    Id: id,
+    Title: title,
     Des: text,
-    Date: date
-  }
+    Date: date,
+  };
 
   allData.push(person);
 
-  list = document.getElementById("list")
+  list = document.getElementById("list");
 
-  let obj = ""
-  
-  for(var i in allData){
-    obj += `<li id=${allData[i].Id}>
+  let obj = "";
+
+  for (var i in allData) {
+    obj += `<li id=${allData[i].Id} onClick="OpenCountdown(${allData[i].Id})">
                 <span>${allData[i].Title}</span>
-                <button onClick="Remove(${allData[i].Id})">Remove</button>
-            </li>`    
+                <button onClick="Remove(${allData[i].Id}); event.stopPropagation();">Remove</button>
+            </li>`;
   }
 
-  list.innerHTML = obj
+  list.innerHTML = obj;
+}
 
-  console.log(allData)
+function Remove(num) {
+  let l = document.getElementById(num);
+  list.removeChild(l);
+
+  let ind = allData.findIndex((v) => v.Id === num);
+  allData.splice(ind, 1);
+}
+
+function OpenCountdown(id) {
+  var result = allData.find((obj) => obj.Id === id);
+  
+  appDiv.style.display = 'none'
+
+  let now = Date.now()
+  let d = parseLocalDate(result.Date)
+
+  console.log(new Date(now))
+  console.log(new Date(d))
+
+  let mil = d - now
+  let time = convertMiliseconds(mil)
+
+  countdownDiv.innerHTML = `<h3>${result.Title}</h3> <br/><br/>
+                            <h1>${time.days}:${time.hours}:${time.minutes}:${time.seconds}</h1>`
+
+  console.log(`${time.days}:${time.hours}:${time.minutes}:${time.seconds}`);
 
 }
 
-function Remove(num){
-    let l = document.getElementById(num)
-    list.removeChild(l);
 
-    let ind = allData.findIndex(v => v.Id === num);
-    allData.splice(ind, 1);
+function convertMiliseconds(ms){
+  let day = Math.floor(ms/ (24 * 3600000))
+  ms %= (24*3600000)
+  let hour = Math.floor(ms/3600000)
+  ms %= 3600000
+  let min = Math.floor(ms/60000)
+  ms %= 60000
+  let sec = Math.floor(ms/1000)
+
+  return {
+    days: day,
+    hours : hour,
+    minutes: min,
+    seconds: sec
+  }
+}
+
+function parseLocalDate(dateString) {
+    const [year, month, day] = dateString.split("-").map(Number);
+    return new Date(year, month - 1, day, 0, 0, 0); // Local midnight
 }
